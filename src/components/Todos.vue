2385468d71c0f9c04d7f4cc0eb68419999e90c55
Vincent Ostyn <vincent.ostyn@gfi.fr>
2019-09-05 17:00:33 +0200

    add selected all

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -2,38 +2,78 @@
     <section class="todoapp">
         <header class="header">
             <h1>Todos</h1>
-            <input type="text" class="new-todo" placeholder="Ajouter une tâche" v-model="newTodo" @keyup.enter="addTodo">
+            <input type="checkbox" v-model="allDone" class="toggle">
+            <input type="text" class="new-todo" placeholder="Ajouter une tâche" v-model="newTodo"
+                   @keyup.enter="addTodo">
         </header>
         <div class="main">
             <ul class="todo-list">
-                <li class="todo" v-for="todo in todos">
+                <li class="todo" v-for="todo in filteredTodos" :class="{completed: todo.completed}">
                     <div class="view">
+                        <input type="checkbox" v-model="todo.completed" class="toggle">
                         <label>{{ todo.name }}</label>
                     </div>
                 </li>
             </ul>
         </div>
+        <footer class="footer">
+            <span class="todo-count"><strong>{{ remaining }}</strong>Tâche à faire</span>
+            <ul class="filters">
+                <li><a href="#" :class="{selected: filter === 'all'}" @click.prevent="filter = 'all'">Toutes</a></li>
+                <li><a href="#" :class="{selected: filter === 'todo'}" @click.prevent="filter = 'todo'">A faire</a></li>
+                <li><a href="#" :class="{selected: filter === 'done'}" @click.prevent="filter = 'done'">Faites</a></li>
+            </ul>
+        </footer>
     </section>
 </template>
 
 <script>
     export default {
-        data () {
+        data() {
             return {
                 todos: [{
-                    name: '',
+                    name: 'Tache de test',
                     completed: false,
                 }],
                 newTodo: '',
+                filter: 'all'
             }
         },
         methods: {
-            addTodo () {
+            addTodo() {
                 this.todos.push({
-                  completed: false,
-                  name: this.newTodo,
+                    completed: false,
+                    name: this.newTodo,
                 })
                 this.newTodo = ''
+            },
+        },
+        computed: {
+            allDone: {
+                get() {
+                    return this.remaining === 0
+                },
+                set(value) {
+                    this.todos.forEach(todo => {
+                        todo.completed = value
+                    })
+                }
+            },
+            remaining() {
+                return this.todos.filter(todo => !todo.completed).length
+                // Ligne du dessus identique à :
+                //    this.todos.filter(function (todo)
+                //    {
+                //        return !todo.completed
+                //    })
+            },
+            filteredTodos() {
+                if (this.filter === 'todo') {
+                    return this.todos.filter(todo => !todo.completed)
+                } else if (this.filter === 'done') {
+                    return this.todos.filter(todo => todo.completed)
+                }
+                return this.todos
             }
         }
     }
